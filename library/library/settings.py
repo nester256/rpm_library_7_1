95b8a8e8f228fda608eb5b40422d97fd0c97031f
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-28 00:33:18 +0300

    ru locale

--- library/library/settings.py
+++ library/library/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
@@ -123,6 +123,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOCALE_PATH = 'library_app/locale'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- library/library_app/models.py
+++ library/library_app/models.py
@@ -1,36 +1,40 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from uuid import uuid4
+from django.utils.translation import gettext_lazy as _
 
 
 class UUIDMixin(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4) # функцию, а не результат
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
 
     class Meta:
         abstract = True
 
 class CreatedMixin(models.Model):
-    created = models.DateTimeField(blank=True, null=True)
+    created = models.DateTimeField(_('created'), blank=True, null=True)
 
     class Meta:
         abstract = True
 
 class ModifiedMixin(models.Model):
-    modified = models.DateTimeField(blank=True, null=True)
+    modified = models.DateTimeField(_('modified'), blank=True, null=True)
 
     class Meta:
         abstract = True
 
 
 class Author(UUIDMixin, CreatedMixin, ModifiedMixin):
-    full_name = models.TextField()
-    books = models.ManyToManyField('Book', through='BookAuthor')
+    full_name = models.TextField(_('full name'))
+    books = models.ManyToManyField('Book', verbose_name=_('books'), through='BookAuthor')
 
     def __str__(self):
         return self.full_name
     
     class Meta:
         db_table = 'author'
+        verbose_name = _('author')
+        verbose_name_plural = _('authors')
+        
 
 def validate_volume(volume: int):
     if volume <= 0:
@@ -41,24 +45,26 @@ def validate_volume(volume: int):
 
 # types of goods
 type_choices = (
-    ('book', 'book'),
-    ('magazine', 'magazine')
+    ('book', _('book')),
+    ('magazine', _('magazine'))
 )
 
 class Book(UUIDMixin, CreatedMixin, ModifiedMixin):
-    title = models.CharField(max_length=40)
-    description = models.TextField(blank=True, null=True)
-    volume = models.IntegerField(validators=[validate_volume])
-    type = models.CharField(max_length=20, choices=type_choices, blank=True, null=True)
-    year = models.IntegerField(blank=True, null=True)
-    authors = models.ManyToManyField(Author, through='BookAuthor')
-    genres = models.ManyToManyField('Genre', through='BookGenre')
+    title = models.CharField(_('title'), max_length=40)
+    description = models.TextField(_('description'), blank=True, null=True)
+    volume = models.IntegerField(_('volume'), validators=[validate_volume])
+    type = models.CharField(_('type'), max_length=20, choices=type_choices, blank=True, null=True)
+    year = models.IntegerField(_('year'), blank=True, null=True)
+    authors = models.ManyToManyField(Author, verbose_name=_('authors'), through='BookAuthor')
+    genres = models.ManyToManyField('Genre', verbose_name=_('genres'), through='BookGenre')
 
     def __str__(self):
         return f'{self.title}, {self.type}, {self.year}.'
 
     class Meta:
         db_table = 'book'
+        verbose_name = _('book')
+        verbose_name_plural = _('books')
 
 
 class BookAuthor(UUIDMixin, CreatedMixin):
@@ -71,21 +77,23 @@ class BookAuthor(UUIDMixin, CreatedMixin):
 
 
 genre_choices = (
-    ('fantasy', 'fantasy'),
-    ('fiction', 'fiction'),
-    ('detective', 'detective')
+    ('fantasy', _('fantasy')),
+    ('fiction', _('fiction')),
+    ('detective', _('detective'))
 )
 
 class Genre(UUIDMixin, CreatedMixin, ModifiedMixin):
-    name = models.CharField(choices=genre_choices, max_length=30)
-    description = models.TextField(blank=True, null=True)
-    books = models.ManyToManyField(Book, through='BookGenre')
+    name = models.CharField(_('name'), choices=genre_choices, max_length=30)
+    description = models.TextField(_('description'), blank=True, null=True)
+    books = models.ManyToManyField(Book, verbose_name=_('books'), through='BookGenre')
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'genre'
+        verbose_name = _('genre')
+        verbose_name_plural = _('genres')
 
 
 class BookGenre(UUIDMixin, CreatedMixin):
