1e0848fcb155902932e7406ea61ac35d147b7274
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-29 23:38:16 +0300

    flake fixed

--- scripts/filling_tables.py
+++ scripts/filling_tables.py
@@ -1,16 +1,15 @@
 from psycopg2 import connect
 from dotenv import load_dotenv
 from os import getenv
-from faker import Faker
 
 
 load_dotenv()
 credentials = {
-    'host' : getenv('PG_HOST'),
-    'port' : int(getenv('PG_PORT')),
-    'user' : getenv('PG_USER'),
-    'password' : getenv('PG_PASSWORD'),
-    'dbname' : getenv('PG_DBNAME')
+    'host': getenv('PG_HOST'),
+    'port': int(getenv('PG_PORT')),
+    'user': getenv('PG_USER'),
+    'password': getenv('PG_PASSWORD'),
+    'dbname': getenv('PG_DBNAME')
 }
 
 genres = {
@@ -18,16 +17,13 @@ genres = {
     'fiction': 'a great song by Tom Odell',
     'detective': 'find out how to run students homework'
 }
-    
+
 connection = connect(**credentials)
 cursor = connection.cursor()
-request = "INSERT INTO library.genre (name, description) VALUES (%s, %s)" 
-# fake = Faker()
+request = "INSERT INTO library.genre (name, description) VALUES (%s, %s)"
 for name, description in genres.items():
-    # full_name: str = str(fake.name())
     cursor.execute(request, (name, description))
 
 connection.commit()
 cursor.close()
 connection.close()
-
--- scripts/make_links.py
+++ scripts/make_links.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 from os import getenv
 from random import choice
 
+
 load_dotenv()
 credentials = {
-    'host' : getenv('PG_HOST'),
-    'port' : int(getenv('PG_PORT')),
-    'user' : getenv('PG_USER'),
-    'password' : getenv('PG_PASSWORD'),
-    'dbname' : getenv('PG_DBNAME')
+    'host': getenv('PG_HOST'),
+    'port': int(getenv('PG_PORT')),
+    'user': getenv('PG_USER'),
+    'password': getenv('PG_PASSWORD'),
+    'dbname': getenv('PG_DBNAME')
 }
 
 connection = connect(**credentials)
@@ -17,28 +18,17 @@ cursor = connection.cursor()
 
 get_ids = 'select id from'
 cursor.execute(f'{get_ids} library.book')
-book_ids = [id[0] for id in cursor.fetchall()]
+book_ids = [id_data[0] for id_data in cursor.fetchall()]
 
 cursor.execute(f'{get_ids} library.genre')
-genre_ids = [id[0] for id in cursor.fetchall()]
+genre_ids = [id_data[0] for id_data in cursor.fetchall()]
 
 request = "INSERT INTO library.book_genre (book_id, genre_id) VALUES ('{0}', '{1}')"
-i = 0
-# with_books = set()
+iteration = 0
 for book_id in book_ids:
     genre_id = choice(genre_ids)
-    # with_books.add(author_id)
     cursor.execute(request.format(book_id, genre_id))
-    print(f'Current progress (with_books): {i / len(book_ids) * 100}%')
-    i += 1
-#
-# without_books = set(author_ids) - with_books
-# i = 0
-# for author_id in without_books:
-#     book_id = choice(book_ids)
-#     cursor.execute(request.format(book_id, author_id))
-#     print(f'Current progress (without_books): {i / len(without_books) * 100}%')
-#     i += 1
+    iteration += 1
 
 connection.commit()
 cursor.close()
