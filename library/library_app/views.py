61f25f7dffa9977efcd5221533df9bbea97f7de2
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-30 14:53:32 +0300

    rest put, delete for setviews

--- library/library_app/views.py
+++ library/library_app/views.py
@@ -6,6 +6,10 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import BookSerializer, AuthorSerializer, GenreSerializer
 from rest_framework.permissions import BasePermission
 from . import config
+from rest_framework import status as status_codes
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.db.models import Model
 
 
 def custom_main(req):
@@ -20,7 +24,7 @@ def custom_main(req):
     )
 
 
-def catalog_view(cls_model, context_name, template):
+def catalog_view(cls_model: Model, context_name, template):
     class CustomListView(ListView):
         model = cls_model
         template_name = template
@@ -69,7 +73,16 @@ class Permission(BasePermission):
         return False
 
 
-def create_viewset(cls_model, serializer, order_field):
+def query_from_request(cls_serializer, request) -> dict:
+    query = {}
+    for param in cls_serializer.Meta.fields:
+        value = request.GET.get(param, '')
+        if value:
+            query[param] = value
+    return query
+
+
+def create_viewset(cls_model: Model, serializer, order_field):
     class CustomViewSet(ModelViewSet):
         serializer_class = serializer
         queryset = cls_model.objects.all().order_by(order_field)
@@ -77,14 +90,56 @@ def create_viewset(cls_model, serializer, order_field):
 
         def get_queryset(self):
             objects = cls_model.objects.all()
-            query = {}
-            for param in serializer.Meta.fields:
-                value = self.request.GET.get(param, '')
-                if value:
-                    query[param] = value
+            query = query_from_request(serializer, self.request)
             if query:
                 objects = objects.filter(**query)
             return objects.order_by(order_field)
+
+        def delete(self, request):
+            query = query_from_request(serializer, request)
+            if query:
+                objects = cls_model.objects.filter(**query)
+                objects_num = len(objects)
+                if not objects_num:
+                    content = f'DELETE query {query} did not match any instances of {cls_model.__name__}'
+                    return Response(content, status=status_codes.HTTP_404_NOT_FOUND)
+                try:
+                    objects.delete()
+                except Exception as error:
+                    return Response(error, status=status_codes.HTTP_500_INTERNAL_SERVER_ERROR)
+                content = f'DELETED {objects_num} instances of {cls_model.__name__}'
+                status = status_codes.HTTP_204_NO_CONTENT if objects_num == 1 else status_codes.HTTP_200_OK
+                return Response(content, status=status)
+            return Response('DELETE has got no query', status=status_codes.HTTP_400_BAD_REQUEST)
+
+        def put(self, request):
+            def serialize(target):
+                content = JSONParser().parse(request)
+                if target:
+                    serialized = serializer(target, data=content, partial=True)
+                    status = status_codes.HTTP_200_OK
+                    body = f'PUT has updated instance of {cls_model.__name__} id={target.id}'
+                else:
+                    serialized = serializer(data=content, partial=True)
+                    status = status_codes.HTTP_201_CREATED
+                    body = f'PUT has created a new instance of {cls_model.__name__}'
+
+                if not serialized.is_valid():
+                    return status_codes.HTTP_400_BAD_REQUEST, f'PUT could not process content: {content}'
+
+                try:
+                    serialized.save()
+                except Exception as error:
+                    return status_codes.HTTP_500_INTERNAL_SERVER_ERROR, error
+                return status, body
+
+            query = query_from_request(serializer, request)
+            target_id = query.get('id', '')
+            if target_id:
+                target_object = cls_model.objects.get(id=target_id)
+                status, body = serialize(target_object)
+                return Response(body, status=status)
+            return Response('PUT has got no id primary key', status=status_codes.HTTP_400_BAD_REQUEST)
 
     return CustomViewSet
 
